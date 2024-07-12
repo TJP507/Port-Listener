fe285476ca3223b18eabf857c89d3fa833589fe2
NCR-S600 <NCR-S600@S600-1>
2024-07-11 21:43:27 -0400

    Add project files.

---
+++ Port Listener/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace TcpListenerApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.rbtnConsole = new System.Windows.Forms.RadioButton();
+            this.rbtnLogFile = new System.Windows.Forms.RadioButton();
+            this.rbtnBoth = new System.Windows.Forms.RadioButton();
+            this.txtLogFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.cmbDataFormat = new System.Windows.Forms.ComboBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rbtnConsole
+            // 
+            this.rbtnConsole.AutoSize = true;
+            this.rbtnConsole.Location = new System.Drawing.Point(12, 12);
+            this.rbtnConsole.Name = "rbtnConsole";
+            this.rbtnConsole.Size = new System.Drawing.Size(134, 17);
+            this.rbtnConsole.TabIndex = 0;
+            this.rbtnConsole.TabStop = true;
+            this.rbtnConsole.Text = "Print Output to Console";
+            this.rbtnConsole.UseVisualStyleBackColor = true;
+            // 
+            // rbtnLogFile
+            // 
+            this.rbtnLogFile.AutoSize = true;
+            this.rbtnLogFile.Location = new System.Drawing.Point(12, 35);
+            this.rbtnLogFile.Name = "rbtnLogFile";
+            this.rbtnLogFile.Size = new System.Drawing.Size(121, 17);
+            this.rbtnLogFile.TabIndex = 1;
+            this.rbtnLogFile.TabStop = true;
+            this.rbtnLogFile.Text = "Log output to log file";
+            this.rbtnLogFile.UseVisualStyleBackColor = true;
+            // 
+            // rbtnBoth
+            // 
+            this.rbtnBoth.AutoSize = true;
+            this.rbtnBoth.Location = new System.Drawing.Point(12, 58);
+            this.rbtnBoth.Name = "rbtnBoth";
+            this.rbtnBoth.Size = new System.Drawing.Size(247, 17);
+            this.rbtnBoth.TabIndex = 2;
+            this.rbtnBoth.TabStop = true;
+            this.rbtnBoth.Text = "Print output to console and log output to log file";
+            this.rbtnBoth.UseVisualStyleBackColor = true;
+            // 
+            // txtLogFilePath
+            // 
+            this.txtLogFilePath.Location = new System.Drawing.Point(12, 81);
+            this.txtLogFilePath.Name = "txtLogFilePath";
+            this.txtLogFilePath.Size = new System.Drawing.Size(276, 20);
+            this.txtLogFilePath.TabIndex = 3;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(294, 79);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 148);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(1013, 377);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(771, 6);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(229, 6);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(40, 20);
+            this.txtPort.TabIndex = 7;
+            this.txtPort.Text = "555";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(771, 35);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // cmbDataFormat
+            // 
+            this.cmbDataFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDataFormat.FormattingEnabled = true;
+            this.cmbDataFormat.Items.AddRange(new object[] {
+            "RAW",
+            "RPOS Security Camera Journal"});
+            this.cmbDataFormat.Location = new System.Drawing.Point(75, 108);
+            this.cmbDataFormat.Name = "cmbDataFormat";
+            this.cmbDataFormat.Size = new System.Drawing.Size(276, 21);
+            this.cmbDataFormat.TabIndex = 9;
+            this.cmbDataFormat.SelectedIndex = 0; // Set default to RAW
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 543);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(1037, 22);
+            this.statusStrip.TabIndex = 10;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(51, 17);
+            this.toolStripStatusLabel.Text = "Stopped";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Listen port:";
+            // this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Log format:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(944, 530);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Version: 1.0.0.0";
+            // this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 565);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.cmbDataFormat);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtLogFilePath);
+            this.Controls.Add(this.rbtnBoth);
+            this.Controls.Add(this.rbtnLogFile);
+            this.Controls.Add(this.rbtnConsole);
+            this.Name = "Form1";
+            this.Text = "Port Listener";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.RadioButton rbtnConsole;
+        private System.Windows.Forms.RadioButton rbtnLogFile;
+        private System.Windows.Forms.RadioButton rbtnBoth;
+        private System.Windows.Forms.TextBox txtLogFilePath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ComboBox cmbDataFormat;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Port Listener/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace TcpListenerApp
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener listener;
+        private bool running = false;
+        private string outputOption = "Console";
+        private string logFilePath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbDataFormat.SelectedIndex = 0; // Set default to RAW
+
+            string currentDir = AppDomain.CurrentDomain.BaseDirectory;
+            string timestamp = DateTime.Now.ToString("MMddyyyy_HHmmss");
+            txtLogFilePath.Text = Path.Combine(currentDir, $"Port-Listener_{timestamp}.log");
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!rbtnConsole.Checked && !rbtnLogFile.Checked && !rbtnBoth.Checked)
+            {
+                MessageBox.Show("Please select an output option.");
+                return;
+            }
+
+            if (rbtnConsole.Checked)
+                outputOption = "Console";
+            else if (rbtnLogFile.Checked)
+                outputOption = "LogFile";
+            else if (rbtnBoth.Checked)
+                outputOption = "Both";
+
+            logFilePath = txtLogFilePath.Text;
+
+            if (outputOption == "LogFile" || outputOption == "Both")
+            {
+                if (string.IsNullOrEmpty(logFilePath))
+                {
+                    MessageBox.Show("Please select a log file path.");
+                    return;
+                }
+            }
+
+            if (!int.TryParse(txtPort.Text, out int port))
+            {
+                MessageBox.Show("Please enter a valid port number.");
+                return;
+            }
+
+            if (IsPortInUse(port))
+            {
+                MessageBox.Show($"Port {port} is already in use. Please select a different port.");
+                return;
+            }
+
+            running = true;
+            UpdateStatus("Running");
+            Task.Run(() => StartListening(port));
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            running = false;
+            listener?.Stop();
+            AppendText("Listener stopped.\n");
+            UpdateStatus("Stopped");
+        }
+
+        private void StartListening(int port)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            AppendText($"Listening on TCP port {port}...\n");
+
+            while (running)
+            {
+                try
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    AppendText("Client connected.\n");
+                    Task.Run(() => HandleClient(client));
+                }
+                catch (SocketException ex) when (ex.SocketErrorCode == SocketError.Interrupted)
+                {
+                    AppendText("\n");
+                }
+                catch (Exception ex)
+                {
+                    AppendText($"Error accepting client: {ex.Message}\n");
+                }
+            }
+
+            listener.Stop();
+        }
+
+        private void HandleClient(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+
+            try
+            {
+                string data = reader.ReadLine();
+                if (!string.IsNullOrEmpty(data))
+                {
+                    string formattedData = FormatData(data);
+                    AppendText($"Received:\n{formattedData}\n");
+
+                    if (outputOption == "LogFile" || outputOption == "Both")
+                    {
+                        File.AppendAllText(logFilePath, $"Received:\n{formattedData}\n");
+                    }
+                }
+                else
+                {
+                    AppendText("No data received.\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendText($"Error receiving data: {ex.Message}\n");
+            }
+            finally
+            {
+                reader.Close();
+                client.Close();
+            }
+        }
+
+        private string FormatData(string data)
+        {
+            StringBuilder formattedData = new StringBuilder();
+            if (cmbDataFormat.SelectedItem.ToString() == "RAW")
+            {
+                formattedData.Append(data);
+            }
+            else if (cmbDataFormat.SelectedItem.ToString() == "RPOS Security Camera Journal")
+            {
+                string[] keyValues = data.Split(',');
+
+                foreach (var kv in keyValues)
+                {
+                    var parts = kv.Split('=');
+                    if (parts.Length == 2)
+                    {
+                        formattedData.AppendLine($"  {parts[0]}: {parts[1]}");
+                    }
+                }
+            }
+
+            return formattedData.ToString();
+        }
+
+        private void AppendText(string text)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(AppendText), text);
+            }
+            else
+            {
+                txtOutput.AppendText(text + Environment.NewLine); // Ensure new lines are properly added
+                txtOutput.SelectionStart = txtOutput.Text.Length; // Scroll to the bottom
+                txtOutput.ScrollToCaret();
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "Log files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    txtLogFilePath.Text = sfd.FileName;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            running = false;
+            listener?.Stop();
+        }
+
+        private bool IsPortInUse(int port)
+        {
+            var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            var tcpConnections = ipGlobalProperties.GetActiveTcpListeners();
+
+            return tcpConnections.Any(endpoint => endpoint.Port == port);
+        }
+
+        private void UpdateStatus(string status)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(UpdateStatus), status);
+            }
+            else
+            {
+                toolStripStatusLabel.Text = status;
+            }
+        }
+    }
+}
